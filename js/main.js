4b1c9049c646a0697759c426ef7eb6d4f9f50ee9
Jeancahu <jeancahu@gmail.com>
2021-10-19 20:10:53 -0600

    add; loading screen example

--- js/main.js
+++ js/main.js
@@ -3,9 +3,13 @@ console.log("hello world!");
 function action (eventbutton) {
 
     // Toggle 'active' class
+    console.log("press button: " + eventbutton.parentNode.id);
 
-    if (eventbutton.parentNode.childNodes[1].classList.contains("active"))
+    if (eventbutton.parentNode.childNodes[1].classList.contains("active")) {
         eventbutton.parentNode.childNodes[1].classList.remove("active");
-    else
+        eventbutton.parentNode.childNodes[1].innerHTML = "inactive";
+    } else {
         eventbutton.parentNode.childNodes[1].classList.add("active");
+        eventbutton.parentNode.childNodes[1].innerHTML = "active";
+    }
 }
